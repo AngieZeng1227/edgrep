41b93abd8e80b06fbd3ead78865a73d08dd5f810
ZZ1227 <43360802+ZZ1227@users.noreply.github.com>
2019-04-10 17:35:46 -0700

    Add files via upload

--- grep.h
+++ grep.h
@@ -9,7 +9,7 @@ int compsub(void);  void dosub(void);  void error(char *s);  int execute(unsigne
 void filename(const char *fname);  void gdelete(void);  char *getblock(unsigned int atl, int iof); int getchr(void);
 int getcopy(void);  int getfile(void);  char *getline_blk(unsigned int tl);  int getnum(void);  int getsub(void);
 int gettty(void);  int gety(void);  void global(int k);  void init(void);
-void join(void);  void move_(int cflag);  void newline(void);  void nonzero(void);  void onhup(int n);
+void join(void);  void move_(int cflag);  void newline(void);  void nonzero(void);
 void onintr(int n);  char *place(char *sp, char *l1, char *l2);  void print(void);  void putchr_(int ac);
 void putd(void);  void putfile(void);  int putline(void);  void puts_(char *sp); void quit(int n);
 void rdelete(unsigned int *ad1, unsigned int *ad2);  void reverse(unsigned int *a1, unsigned int *a2);
